b906c541dde1934fa884ef4b522b49308568385e
koto74 <kotoganaiyo@gmail.com>
2024-02-17 10:14:18 +0900

    chore: ファイル名修正

--- todo-app/main.go
+++ todo-app/main.go
@@ -5,9 +5,10 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
+	data := "hello Go/gin!!"
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{})
+		c.HTML(200, "index.html", gin.H{"data": data})
 	})
 
 	router.Run(":8080")
