4e553929f8150b74b889f7c148cd1b0b01035476
koto74 <kotoganaiyo@gmail.com>
2024-02-22 11:06:39 +0900

    chore: Dockerfile周り修正

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,16 +1,9 @@
 package main
 
-<<<<<<< HEAD
 import (
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-=======
-import "fmt"
-
-func main() {
-	fmt.Println("Hello World")
->>>>>>> origin/master
 }
